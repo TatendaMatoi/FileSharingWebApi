f1135541271c855db9055f9b5fbe3465ec5ec997
TatendaMatoi <h200230b@hit.ac.zw>
2025-01-22 07:12:56 +0200

    Added initial database migration and UsersController for API endpoin

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using FileSharingWebApi.Models;
+using FileSharingWebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileSharingWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly FileSharingDbContext _context;
+        public UsersController(FileSharingDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
