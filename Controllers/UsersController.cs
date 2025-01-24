7819b7ecfe7727379020692e184999e5a3069739
TatendaMatoi <h200230b@hit.ac.zw>
2025-01-24 14:31:20 +0200

    Added update user method and logic to UserController.cs file.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using FileSharingWebApi.Models;
 using FileSharingWebApi.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FileSharingWebApi.Controllers
 {
@@ -9,9 +10,66 @@ namespace FileSharingWebApi.Controllers
     public class UsersController : ControllerBase
     {
         private readonly FileSharingDbContext _context;
+
         public UsersController(FileSharingDbContext context)
         {
             _context = context;
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
     }
 }
